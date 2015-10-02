cff44dd7332c7392ae24135ef63c1617ad8eb9ca
mhaii <bloo20xd@gmail.com>
2015-10-02 17:48:14 +0700

    add tap loop

--- main.cpp
+++ main.cpp
@@ -3,34 +3,50 @@
 
 #include "keylist.h"
 
-#define button num_9
+#define button spacebar
 #define tps 40
-#define tpr 1.0/tps
+#define tpr 1000.0/tps
 
 using namespace std;
 
-int init();
+int init(int);
+int tap();
+inline void sleep(DWORD sec) { Sleep(sec*1000); }
 
 HWND hwnd;
 
 int main() {
-
+    tap();
 }
 
-int init(int count = 0){
+int tap(){
+    while (init(0)){
+        while (hwnd){
+            if (GetForegroundWindow() == hwnd) {
+                while (GetKeyState(VK_LBUTTON) < 0);
+            }
+            PostMessage(hwnd, WM_KEYDOWN, button, 0);
+            PostMessage(hwnd, WM_KEYUP, button, 0);
+            Sleep(tpr);
+        }
+    }
+}
+
+int init(int count){
     cout << "Acquiring BlueStacks hwnd" << endl;
     hwnd = FindWindow(NULL, "BlueStacks App Player");
-    Sleep(1000);
+    sleep(1);
     if (hwnd == 0){
         cout << "Failed to acquired hwnd (or BlueStacks isn't opened)" << endl;
-        if (count > 4) return -1;
+        if (count > 4) return 0;
         cout << "Retry in 5 seconds";
-        for (int i = 0; i < 4; i++) { cout << "."; Sleep(1000); } cout << endl;
-        init(count+1);
+        for (int i = 0; i < 4; i++) { cout << "."; sleep(1); } cout << endl;
+        return init(count+1);
     } else {
         cout << "Got " << hwnd << " as hwnd of BlueStacks" << endl;
         cout << "Spamming [" << button << "] to BlueStacks" << endl;
         cout << "Rate of tap is ("  << tps << ") tap per second" << endl;
-        cout << "Tap every {" << tpr << "} second" << endl;
+        cout << "Tap every {" << tpr << "} miliseconds" << endl;
+        return 1;
     }
 }
