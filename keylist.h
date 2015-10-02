98284aec6c3d4ac53c0fff06f6c10902b761af68
mhaii <bloo20xd@gmail.com>
2015-10-02 15:26:34 +0700

    add list of key values

---
+++ keylist.h
@@ -0,0 +1,99 @@
+#ifndef BLUESTACK_CLICKER_KEYLIST_H
+#define BLUESTACK_CLICKER_KEYLIST_H
+
+#define backspace 0x08
+#define tab 0x09
+#define clear 0x0C
+#define enter 0x0D
+
+#define shift 0x10
+#define ctrl 0x11
+#define alt 0x12
+
+#define pause 0x13
+#define caps_lock 0x14
+#define esc 0x1B
+#define spacebar 0x20
+#define page_up 0x21
+#define page_down 0x22
+#define end 0x23
+#define home 0x24
+#define print_screen 0x2C
+#define ins 0x2D
+#define del 0x2E
+#define help 0x2F
+
+#define left_arrow 0x25
+#define up_arrow 0x26
+#define right_arrow 0x27
+#define down_arrow 0x28
+
+#define key_0 0x30
+#define key_1 0x31
+#define key_2 0x32
+#define key_3 0x33
+#define key_4 0x34
+#define key_5 0x35
+#define key_6 0x36
+#define key_7 0x37
+#define key_8 0x38
+#define key_9 0x39
+#define key_a 0x41
+#define key_b 0x42
+#define key_c 0x43
+#define key_d 0x44
+#define key_e 0x45
+#define key_f 0x46
+#define key_g 0x47
+#define key_h 0x48
+#define key_i 0x49
+#define key_j 0x4A
+#define key_k 0x4B
+#define key_l 0x4C
+#define key_m 0x4D
+#define key_n 0x4E
+#define key_o 0x4F
+#define key_p 0x50
+#define key_q 0x51
+#define key_r 0x52
+#define key_s 0x53
+#define key_t 0x54
+#define key_u 0x55
+#define key_v 0x56
+#define key_w 0x57
+#define key_x 0x58
+#define key_y 0x59
+#define key_z 0x5A
+
+#define num_0 0x60
+#define num_1 0x61
+#define num_2 0x62
+#define num_3 0x63
+#define num_4 0x64
+#define num_5 0x65
+#define num_6 0x66
+#define num_7 0x67
+#define num_8 0x68
+#define num_9 0x69
+#define multiply_key 0x6A
+#define add_key 0x6B
+#define separator_key 0x6C
+#define subtract_key 0x6D
+#define decimal_key 0x6E
+#define divide_key 0x6F
+
+#define F1 0x70
+#define F2 0x71
+#define F3 0x72
+#define F4 0x73
+#define F5 0x74
+#define F6 0x75
+#define F7 0x76
+#define F8 0x77
+#define F9 0x78
+#define F10 0x79
+#define F11 0x7A
+#define F12 0x7B
+}
+
+#endif //BLUESTACK_CLICKER_KEYLIST_H
